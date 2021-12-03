9ed4e3d2456f70ad0c8e54ef380b0ec2655eda15
patrick mugambi <patrick@kali.pm.com>
2021-12-03 17:55:53 +0300

    set up model to define post and folder configurations for images

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User 
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    """
+    Post model class to define post database table
+    """
+    description = models.TextField()
+    image = models.ImageField(upload_to='uploads/')
+    title = models.CharField(max_length=100, blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    user_posted = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes=models.IntegerField(null=True, default=0)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('homepage-index')
+    
--- instagram/settings.py
+++ instagram/settings.py
@@ -132,3 +132,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'homepage:index'
 
 LOGOUT_REDIRECT_URL = "homepage:index"
+#media 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+#static files
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR, 'static']
