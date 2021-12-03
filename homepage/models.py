937eb99321abdff87fa838359042fbbafa72b479
patrick mugambi <patrick@kali.pm.com>
2021-12-03 17:59:09 +0300

    comment model

--- homepage/models.py
+++ homepage/models.py
@@ -19,4 +19,16 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('homepage-index')
+     @property
+    def number_of_comments(self):
+        return Comment.objects.filter(post=self).count()
     
+class Comment(models.Model):
+    """
+    class to create the comment model
+    """
+
+    description = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
