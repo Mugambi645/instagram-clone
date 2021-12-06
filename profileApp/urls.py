9752006121f06ab6a4e9594e52109c20ef4d8ff2
patrick mugambi <patrick@kali.pm.com>
2021-12-06 10:34:43 +0300

    readme

--- profileApp/urls.py
+++ profileApp/urls.py
@@ -3,8 +3,10 @@ from . import views
 app_name = "profileApp"
 urlpatterns = [
        path('profile/', views.profile, name='profile'),
+       
        path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
        path('follow/<to_follow>', views.follow, name='follow'),
        path('search/', views.search_profile, name='search'),
+       #path("all/",views.all_profiles, name="all_profiles"),
        path('user_profile/<username>/', views.user_profile, name='user_profile'),
 ]
--- profileApp/views.py
+++ profileApp/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required 
 from .forms import UserUpdateForm, ProfileUpdateForm
 from homepage.models import Post,Comment
 from .models import Profile,Follow
+from django.contrib.auth.models import User
 # Create your views here.
 
 # Update it here
