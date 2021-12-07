b8ac5db1efa62505bb8683d214e0aa76cc329c47
Mugambi Patrick <47353761+Mugambi645@users.noreply.github.com>
2021-12-07 09:58:30 +0300

    Update tests.py

--- profileApp/tests.py
+++ profileApp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Profile
 
-# Create your tests here.
+class ProfileTestClass(TestCase):
+  """
+  model tests to test profile class functionality
+  """
+  def setUp(self):
+    self.james= Profile(user = "john", image ='profile.jpg')
+  def test_instance(self):
+        self.assertTrue(isinstance(self.james,Profile))
